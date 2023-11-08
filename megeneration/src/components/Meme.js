a830f22d690efb846cbaafafc2d0b3cd5c2a5a11
WM <wsuan15@gmail.com>
2023-11-08 08:54:06 +0800

    push

--- megeneration/src/components/Meme.js
+++ megeneration/src/components/Meme.js
@@ -2,15 +2,23 @@ import React from "react";
 import data1 from "./memedata"
 export default function Meme(){
    
-    const [memeImage,setMemeImage] =React.useState("")
+    // const [memeImage,setMemeImage] =React.useState("https://www.w3schools.com/images/w3schools_green.jpg")
+    const [meme, setMeme]=React.useState({
+        topText:"",
+        bottomText:"",
+        randomImage:"https://www.w3schools.com/images/w3schools_green.jpg"
+    })
+    const [allMemeImages,setAllMemeImages]=React.useState(data1)
 
     function getMemeimage(){
-        const memesArray =data1.data.memes
+        const memesArray =allMemeImages.data.memes
         // const randomNumber = Math.floor(Math.rondom() * memesArray.length)
         const randomNumber = Math.floor(Math.random() * 2);
         const url =memesArray[randomNumber].url
-        console.log(url);
-        setMemeImage(memesArray[randomNumber].url)
+        // console.log(url);
+        setMeme(preMeme =>({
+            ...preMeme,randomImage:url
+        }))
     }
     return(
         <main>
@@ -32,7 +40,7 @@ export default function Meme(){
                     Get a new meme image
                 </button>
             </div>
-            <img src={memeImage} className="meme-image"/>
+            <img src={meme.randomImage} className="meme-image"/>
         </main>
     )
 
